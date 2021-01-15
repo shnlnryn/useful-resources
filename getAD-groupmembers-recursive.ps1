0b384702f314e3156654d8cd210f168f2cb934a8
Shaneil <53551518+shnlnryn@users.noreply.github.com>
2021-01-15 11:16:35 +1100

    Create getAD-groupmembers-recursive.ps1

---
+++ getAD-groupmembers-recursive.ps1
@@ -0,0 +1,13 @@
+#Get groups starting with BI-*
+$g= get-adgroup -Filter {name -like "BI-*"} -Properties * |select name, description, SamAccountName
+$g | export-csv -Path d:\temp\ALL-Bi-groups.csv -NoTypeInformation
+
+
+#Recursively get all members of the groups from above
+foreach ($group in $g){
+$group.name
+$filename = "d:\temp\BI\" + $group.name + ".csv"
+
+Get-ADGroupmember -identity $group.name -Recursive| % { get-aduser $_.samaccountname | select name,userprincipalname } | export-csv -path $filename -notypeinformation
+
+}
